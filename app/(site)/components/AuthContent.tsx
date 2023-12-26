8d9ad3181016aaa587a481b0c80b14cc19f70ca2
Singulariity <Singulariity@users.noreply.github.com>
2023-12-26 18:37:48 +0200

    Update AuthContent.tsx

--- app/(site)/components/AuthContent.tsx
+++ app/(site)/components/AuthContent.tsx
@@ -13,7 +13,7 @@ import { useState } from 'react';
 
 type LoginType = 'advisor' | 'student';
 
-const Page = () => {
+export default function Page() {
 	const [loginType, setLoginType] = useState<LoginType>('advisor');
 	const { token } = theme.useToken();
 	const [messageApi, contextHolder] = message.useMessage();
@@ -42,114 +42,108 @@ const Page = () => {
 	}
 
 	return (
-		<div
-			style={{
-				backgroundColor: 'black',
-				height: '100vh',
-			}}
-		>
-			{contextHolder}
-			<LoginFormPage
-				backgroundImageUrl="https://i.imgur.com/4CIe1Nn.jpg"
-				backgroundVideoUrl="https://gw.alipayobjects.com/v/huamei_gcee1x/afts/video/jXRBRK_VAwoAAAAAAAAAAAAAK4eUAQBr"
-				title="Login"
-				containerStyle={{
-					backgroundColor: 'rgba(0, 0, 0,0.65)',
-					backdropFilter: 'blur(4px)',
-				}}
-				subTitle="..."
-				onFinish={submitHandler}
-			>
-				<Tabs
-					centered
-					activeKey={loginType}
-					onChange={(activeKey) =>
-						setLoginType(activeKey as LoginType)
-					}
-				>
-					<Tabs.TabPane key={'advisor'} tab={'Advisor'} />
-					<Tabs.TabPane key={'student'} tab={'Student'} />
-				</Tabs>
-				{loginType === 'advisor' && (
-					<>
-						<ProFormText
-							name="username"
-							fieldProps={{
-								size: 'large',
-								prefix: (
-									<UserOutlined
-										style={{
-											color: token.colorText,
-										}}
-										className={'prefixIcon'}
-									/>
-								),
-							}}
-							placeholder={'Username'}
-							rules={[
-								{
-									required: true,
-									message: 'This field is required!',
-								},
-							]}
-						/>
-						<ProFormText.Password
-							name="password"
-							fieldProps={{
-								size: 'large',
-								prefix: (
-									<LockOutlined
-										style={{
-											color: token.colorText,
-										}}
-										className={'prefixIcon'}
-									/>
-								),
-							}}
-							placeholder={'Password'}
-							rules={[
-								{
-									required: true,
-									message: 'This field is required!',
-								},
-							]}
-						/>
-					</>
-				)}
-				{loginType === 'student' && (
-					<>
-						<ProFormText
-							name="student_no"
-							fieldProps={{
-								size: 'large',
-								prefix: (
-									<UserOutlined
-										style={{
-											color: token.colorText,
-										}}
-										className={'prefixIcon'}
-									/>
-								),
-							}}
-							placeholder={'Student Number'}
-							rules={[
-								{
-									required: true,
-									message: 'This field is required!',
-								},
-							]}
-						/>
-					</>
-				)}
-			</LoginFormPage>
-		</div>
-	);
-};
-
-export default () => {
-	return (
 		<ProConfigProvider dark>
-			<Page />
+			{contextHolder}
+			<div
+				style={{
+					backgroundColor: 'black',
+					height: '100vh',
+				}}
+			>
+				<LoginFormPage
+					backgroundImageUrl="https://i.imgur.com/4CIe1Nn.jpg"
+					backgroundVideoUrl="https://gw.alipayobjects.com/v/huamei_gcee1x/afts/video/jXRBRK_VAwoAAAAAAAAAAAAAK4eUAQBr"
+					title="Login"
+					containerStyle={{
+						backgroundColor: 'rgba(0, 0, 0,0.65)',
+						backdropFilter: 'blur(4px)',
+					}}
+					subTitle="..."
+					onFinish={submitHandler}
+				>
+					<Tabs
+						centered
+						activeKey={loginType}
+						onChange={(activeKey) =>
+							setLoginType(activeKey as LoginType)
+						}
+					>
+						<Tabs.TabPane key={'advisor'} tab={'Advisor'} />
+						<Tabs.TabPane key={'student'} tab={'Student'} />
+					</Tabs>
+					{loginType === 'advisor' && (
+						<>
+							<ProFormText
+								name="username"
+								fieldProps={{
+									size: 'large',
+									prefix: (
+										<UserOutlined
+											style={{
+												color: token.colorText,
+											}}
+											className={'prefixIcon'}
+										/>
+									),
+								}}
+								placeholder={'Username'}
+								rules={[
+									{
+										required: true,
+										message: 'This field is required!',
+									},
+								]}
+							/>
+							<ProFormText.Password
+								name="password"
+								fieldProps={{
+									size: 'large',
+									prefix: (
+										<LockOutlined
+											style={{
+												color: token.colorText,
+											}}
+											className={'prefixIcon'}
+										/>
+									),
+								}}
+								placeholder={'Password'}
+								rules={[
+									{
+										required: true,
+										message: 'This field is required!',
+									},
+								]}
+							/>
+						</>
+					)}
+					{loginType === 'student' && (
+						<>
+							<ProFormText
+								name="student_no"
+								fieldProps={{
+									size: 'large',
+									prefix: (
+										<UserOutlined
+											style={{
+												color: token.colorText,
+											}}
+											className={'prefixIcon'}
+										/>
+									),
+								}}
+								placeholder={'Student Number'}
+								rules={[
+									{
+										required: true,
+										message: 'This field is required!',
+									},
+								]}
+							/>
+						</>
+					)}
+				</LoginFormPage>
+			</div>
 		</ProConfigProvider>
 	);
-};
+}
